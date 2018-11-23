4370638c87f20a11f48ee5f5937b1d5a7d01d510
EmirYilmazturk <44133479+EmirYilmazturk@users.noreply.github.com>
2018-11-23 11:03:35 +0300

    Update steamstore.js

--- komutlar/steamstore.js
+++ komutlar/steamstore.js
@@ -5,7 +5,7 @@ var provider = new steam.SteamProvider();
 exports.run = (client, message, args) => {
     let game = args[0]
     let steampng = "https://cdn.discordapp.com/attachments/458004691402489856/470344660364034049/steam.png"
-    if (!game) return message.reply('Lütfen Steamde Olan Bir Oyunun Adını Yazın. Örnek: `!steamstore portal 2`')
+    if (!game) return message.reply('Lütfen Steamde Olan Bir Oyunun Adını Yazın. Örnek: `e!steamstore CS:GO`')
     provider.search(game).then(result => {
     provider.detail(result[0].id, "turkey", "tr").then(results => {
         console.log(results)
